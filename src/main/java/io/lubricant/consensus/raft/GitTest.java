88db7654728fe905cd3e43e7c54809816bd0dde9
wangchao <wangchao001@zhongan.com>
2024-08-08 16:46:07 +0800

    add 8

--- src/main/java/io/lubricant/consensus/raft/GitTest.java
+++ src/main/java/io/lubricant/consensus/raft/GitTest.java
@@ -29,5 +29,6 @@ public class GitTest {
         System.out.println("Hello World!");
         System.out.println("Hello World!");
         System.out.println("Hello World!");
+        System.out.println("Hello World!");
     }
 }
